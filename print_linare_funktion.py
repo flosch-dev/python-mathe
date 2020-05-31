a6d78ed21ec7a7b7a911700fd4bcf507b9d0de8c
Florian Schilling <flosch.devel@gmail.com>
2020-06-01 01:18:40 +0200

    pylint correction

--- print_linare_funktion.py
+++ print_linare_funktion.py
@@ -21,6 +21,7 @@ for werte in sys.argv[1:][0].split():
 
 
 def steigung(PUNKTE):
+    """ steigung ermitteln """
     M=abs(PUNKTE["Y"][0] - PUNKTE["Y"][1]) / abs(PUNKTE["X"][0] - PUNKTE["X"][1])
 
     if (PUNKTE["X"][0] < PUNKTE["X"][1] and PUNKTE["Y"][0] < PUNKTE["Y"][1]) or (PUNKTE["X"][0] > PUNKTE["X"][1] and PUNKTE["Y"][0] > PUNKTE["Y"][1]):
@@ -31,6 +32,7 @@ def steigung(PUNKTE):
         return ["fallender Graph", M*-1]
 
 def isInt(WERT):
+    """ prüft ob wert float oder int ist """
     if str(WERT).endswith(".0"):
         return int(WERT)
     else:
