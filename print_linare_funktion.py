3956bc8b70ac2d1b7d2473b7fb65df7bb5ab0686
Florian Schilling <flosch.devel@gmail.com>
2020-06-01 00:59:39 +0200

    update indentation

--- print_linare_funktion.py
+++ print_linare_funktion.py
@@ -4,9 +4,9 @@ import sys
 import matplotlib.pyplot as plt
 
 try:
-  sys.argv[1:]
+    sys.argv[1:]
 except:
-  print("missing arguments: \"1x|1y 2x|2y\"")
+    print("missing arguments: \"1x|1y 2x|2y\"")
 
 
 PUNKTE      = {}
@@ -15,27 +15,26 @@ PUNKTE["Y"] = []
 
 
 for werte in sys.argv[1:][0].split():
-  WERT=werte.split("|")
-  PUNKTE["X"].append(float(WERT[0]))
-  PUNKTE["Y"].append(float(WERT[1]))
+    WERT=werte.split("|")
+    PUNKTE["X"].append(float(WERT[0]))
+    PUNKTE["Y"].append(float(WERT[1]))
 
 
 def steigung(PUNKTE):
-  M=abs(PUNKTE["Y"][0] - PUNKTE["Y"][1]) / abs(PUNKTE["X"][0] - PUNKTE["X"][1])
+    M=abs(PUNKTE["Y"][0] - PUNKTE["Y"][1]) / abs(PUNKTE["X"][0] - PUNKTE["X"][1])
 
-  if (PUNKTE["X"][0] < PUNKTE["X"][1] and PUNKTE["Y"][0] < PUNKTE["Y"][1]) or (PUNKTE["X"][0] > PUNKTE["X"][1] and PUNKTE["Y"][0] > PUNKTE["Y"][1]):
-    return ["steigenger Graph",M]
-  elif PUNKTE["X"][0] == PUNKTE["X"][1] and PUNKTE["Y"] != PUNKTE["Y"]:
-    return ["gleichbleibender Graph",0]
-  else:
-    return ["fallender Graph",M*-1]
-
+    if (PUNKTE["X"][0] < PUNKTE["X"][1] and PUNKTE["Y"][0] < PUNKTE["Y"][1]) or (PUNKTE["X"][0] > PUNKTE["X"][1] and PUNKTE["Y"][0] > PUNKTE["Y"][1]):
+        return ["steigenger Graph",M]
+    elif PUNKTE["X"][0] == PUNKTE["X"][1] and PUNKTE["Y"] != PUNKTE["Y"]:
+        return ["gleichbleibender Graph",0]
+    else:
+        return ["fallender Graph",M*-1]
 
 def isInt(WERT):
-  if str(WERT).endswith(".0"):
-    return int(WERT)
-  else:
-    return float(WERT)
+    if str(WERT).endswith(".0"):
+        return int(WERT)
+    else:
+        return float(WERT)
 
 M = steigung(PUNKTE)
 
