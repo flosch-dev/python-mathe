978cb1cab3603f54fab7555e60a69c0d0034cc35
Florian Schilling <flosch.devel@gmail.com>
2020-06-01 01:55:25 +0200

    .

--- print_linare_funktion.py
+++ print_linare_funktion.py
@@ -20,23 +20,23 @@ for werte in sys.argv[1:][0].split():
     PUNKTE["Y"].append(float(WERT[1]))
 
 
-def steigung(PUNKTE):
+def steigung(punkte):
     """ steigung ermitteln """
-    M=abs(PUNKTE["Y"][0] - PUNKTE["Y"][1]) / abs(PUNKTE["X"][0] - PUNKTE["X"][1])
+    M = abs(punkte["Y"][0] - punkte["Y"][1]) / abs(punkte["X"][0] - punkte["X"][1])
 
-    if (PUNKTE["X"][0] < PUNKTE["X"][1] and PUNKTE["Y"][0] < PUNKTE["Y"][1]) or (PUNKTE["X"][0] > PUNKTE["X"][1] and PUNKTE["Y"][0] > PUNKTE["Y"][1]):
+    if (punkte["X"][0] < punkte["X"][1] and punkte["Y"][0] < punkte["Y"][1]) or (punkte["X"][0] > punkte["X"][1] and punkte["Y"][0] > punkte["Y"][1]):
         return ["steigenger Graph", M]
-    elif PUNKTE["X"][0] == PUNKTE["X"][1] and PUNKTE["Y"] != PUNKTE["Y"]:
+    elif punkte["X"][0] == punkte["X"][1] and punkte["Y"] != punkte["Y"]:
         return ["gleichbleibender Graph", 0]
     else:
         return ["fallender Graph", M*-1]
 
-def isInt(WERT):
+def isInt(wert):
     """ prüft ob wert float oder int ist """
-    if str(WERT).endswith(".0"):
-        return int(WERT)
+    if str(wert).endswith(".0"):
+        return int(wert)
     else:
-        return float(WERT)
+        return float(wert)
 
 M = steigung(PUNKTE)
 
